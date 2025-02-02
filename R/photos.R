3a8a800c42d9ec346eb5152be104ac292131da54
Setonix <setonix.shibari@gmail.com>
2025-02-02 14:43:25 +0100

    grid corsets

--- R/photos.R
+++ R/photos.R
@@ -1,16 +1,34 @@
-dir <- "www/images/ties"
-cats <- ""
+dir <- "www/images/grid"
+cats <- "grid; corset; nsfw"
 
 
-images <- list.files(dir, recursive = TRUE, full.names = TRUE)
+images <- list.files(dir, "[1-6]{2}", recursive = TRUE, full.names = TRUE)
 for (img in images) {
   base <- basename(img) |> sub("\\.jpg$", "", x = _)
-  file <- paste0("photos/", base, ".qmd")
+  file <- paste0("photos/grid-", base, ".qmd")
   
-  if (!file.exists(file)) {
+  if (file.exists(file)) {
     title <- base |> 
       gsub("-", " ", x = _) |> 
       stringr::str_to_title()
+    
+    title <- paste("Grid Corset with", dplyr::case_match(substr(base, 4, 4), 
+                               "r" ~ "Red",
+                               "o" ~ "Orange",
+                               "y" ~ "Yellow",
+                               "g" ~ "Green",
+                               "b" ~ "Blue",
+                               "p" ~ "Purple"),
+                   "Base and",
+                   dplyr::case_match(substr(base, 6, 6), 
+                                     "r" ~ "Red",
+                                     "o" ~ "Orange",
+                                     "y" ~ "Yellow",
+                                     "g" ~ "Green",
+                                     "b" ~ "Blue",
+                                     "p" ~ "Purple"),
+                   "Boning")
+    
 
     txt <- sprintf("---
 title: %s
@@ -18,6 +36,8 @@ categories: [photo%s]
 image: ../%s
 image-alt: \"\"
 ---
+
+Corset from the [grid project](/posts/2025-01-27_grid-project.html).
 
 ![](../%s)
 ", title, cats, img, img)
