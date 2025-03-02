47c5f7c55e54a95bfa92e3bd6ce211f2ae7fde8a
Setonix <setonix.shibari@gmail.com>
2025-03-02 11:49:44 +0000

    more photos

--- R/photos.R
+++ R/photos.R
@@ -1,34 +1,36 @@
-dir <- "www/images/grid-outtakes"
-cats <- ", grid, corset, nsfw"
+dir <- "www/images/new"
+cats <- ", nsfw"
 
 
-images <- list.files(dir, "\\d-\\d", recursive = TRUE, full.names = TRUE)
-images <- images[!grepl("1-1|2-2|3-3|4-4|5-5|6-6", images)]
+images <- list.files(dir, ".", recursive = TRUE, full.names = TRUE)
+images <- gsub(dir, "www/images/ties", images)
+#images <- images[!grepl("1-1|2-2|3-3|4-4|5-5|6-6", images)]
 for (img in images) {
   base <- basename(img) |> sub("\\.jpg$", "", x = _)
-  file <- paste0("photos/grid-outtake-", base, ".qmd")
+  file <- paste0("photos/", base, ".qmd")
+  
   
   if (!file.exists(file)) {
     title <- base |> 
       gsub("-", " ", x = _) |> 
       stringr::str_to_title()
-    
-    title <- paste("Grid Corset with", dplyr::case_match(substr(base, 1, 1), 
-                               "1" ~ "Red",
-                               "2" ~ "Orange",
-                               "3" ~ "Yellow",
-                               "4" ~ "Green",
-                               "5" ~ "Blue",
-                               "6" ~ "Purple"),
-                   "Base and",
-                   dplyr::case_match(substr(base, 3, 3), 
-                                     "1" ~ "Red",
-                                     "2" ~ "Orange",
-                                     "3" ~ "Yellow",
-                                     "4" ~ "Green",
-                                     "5" ~ "Blue",
-                                     "6" ~ "Purple"),
-                   "Boning")
+
+      # title <- paste("Grid Corset with", dplyr::case_match(substr(base, 1, 1), 
+      #                          "1" ~ "Red",
+      #                          "2" ~ "Orange",
+      #                          "3" ~ "Yellow",
+      #                          "4" ~ "Green",
+      #                          "5" ~ "Blue",
+      #                          "6" ~ "Purple"),
+      #              "Base and",
+      #              dplyr::case_match(substr(base, 3, 3), 
+      #                                "1" ~ "Red",
+      #                                "2" ~ "Orange",
+      #                                "3" ~ "Yellow",
+      #                                "4" ~ "Green",
+      #                                "5" ~ "Blue",
+      #                                "6" ~ "Purple"),
+      #              "Boning")
     
 
     txt <- sprintf("---
@@ -38,7 +40,6 @@ image: ../%s
 image-alt: \"\"
 ---
 
-Outtake of corset from the [grid project](/posts/2025-01-27_grid-project.html).
 
 ![](../%s)
 ", title, cats, img, img)
