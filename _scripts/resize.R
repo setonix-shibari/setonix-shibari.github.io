a71bec7f2443b9365d13b4737704bcd1e8054ef4
Setonix <setonix.shibari@gmail.com>
2025-06-07 11:04:54 +0100

    uncensored

--- _scripts/resize.R
+++ _scripts/resize.R
@@ -1,7 +1,7 @@
 library(webmorphR)
 
 odir <- "../s/new/"
-ndir <- "www/images/ties/"
+ndir <- "www/images/uncensored/"
 orig <- read_stim(odir, "\\.jpg")
 resized <- resize(orig, height = 1000)
 write_stim(resized, ndir, format = "jpg")
