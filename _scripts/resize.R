5faa21f387f1ccc85902e0e5930ffe98606f91e9
Setonix <setonix.shibari@gmail.com>
2025-08-29 15:15:25 +0100

    new tutorial

--- _scripts/resize.R
+++ _scripts/resize.R
@@ -1,7 +1,7 @@
 library(webmorphR)
 
 odir <- "../s/new/"
-ndir <- "www/images/uncensored/"
+ndir <- "www/images/nnnn/"
 orig <- read_stim(odir, "\\.jpg")
 resized <- resize(orig, height = 1000)
 write_stim(resized, ndir, format = "jpg")
